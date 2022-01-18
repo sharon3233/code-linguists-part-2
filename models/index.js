0081ff73ed4b39687e2b347a4def5a0e3dcdc96f
sharon3233 <shalaskin29@gmail.com>
2022-01-18 14:19:17 -0600

    first commit

---
+++ models/index.js
@@ -0,0 +1,29 @@
+const {Sequelize, DataTypes, Model} = require('sequelize')
+const {sequelize} = require('../db')
+
+
+class User extends Model {}
+
+class Entry extends Model {}
+
+User.init({
+    name: DataTypes.STRING,
+    password: DataTypes.STRING,
+}, {
+    sequelize,
+    timestamps: false,
+});
+
+
+Entry.init({
+    name: DataTypes.STRING,
+    body: DataTypes.STRING,
+}, {
+    sequelize,
+    timestamps: false,
+});
+
+User.hasMany(Entry);
+Entry.belongsTo(User);
+
+module.exports = {User, Entry};
